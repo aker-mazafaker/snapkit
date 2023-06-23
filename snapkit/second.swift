27d4fb646efc8206d515cdadab29d4ba8e0919f5
Aker <@akerke.n@inbox.ru>
2023-06-23 22:25:55 +0600

    add second

---
+++ snapkit/second.swift
@@ -0,0 +1,33 @@
+//
+//  second.swift
+//  snapkit
+//
+//  Created by Akerke on 23.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+class SecondViewControler: UIViewController {
+    
+    var name: String = ""
+    
+    let myLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(myLabel)
+        myLabel.snp.makeConstraints{
+        $0.center.equalToSuperview()
+        
+        }
+        myLabel.text = "Hello, \(name)"
+    }
+}
+
+
